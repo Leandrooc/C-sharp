e1071b7b7383f233fe8da0a41ac7f6510b2b7ceb
unknown <19leandro3quietmsl@gmail.com>
2022-05-20 02:07:12 -0300

    feat: First Program with C#;

---
+++ basic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace firstProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Escreva um número inteiro");
+            int inputUser = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite seu nome");
+            string name = Console.ReadLine();
+            getOddNumbers(inputUser, name);
+        }
+        static void getOddNumbers(int number, string name)
+        {
+            int odds = 0;
+            for (int a = 1; a <= number; a++)
+            {
+                if ((a / 2) * 2 != a)
+                {
+                    Console.WriteLine(a);
+                    odds++;
+                }
+            }
+            Console.WriteLine(name + ", Existem " + odds + " números impares: ");
+        }
+    }
+}
